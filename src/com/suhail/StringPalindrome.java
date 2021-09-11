42765e94b12e55c0fedfe099d2928c29e3b960e5
Suhail Khan <suhailkna@gmail.com>
2021-09-11 22:49:41 +0530

    Solved String Palindrome Question

---
+++ src/com/suhail/StringPalindrome.java
@@ -0,0 +1,25 @@
+package com.suhail;
+
+import java.util.Locale;
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        String str = "abcdba";
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String str) {
+
+        if(str == null || str.length() == 0){
+            return true;
+        }
+
+        str = str.toLowerCase();
+        for(int idx = 0; idx < str.length(); idx++){
+            if(str.charAt(idx) != str.charAt(str.length() - 1 - idx)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
